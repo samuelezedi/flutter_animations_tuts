b4b9497ffe8f0a608d5a725a17dcf322ce46e4ef
macbookpro <12Toronto@>
2021-02-06 11:17:28 +0100

    implemented video player and chewie, still developing

--- lib/flutter_video_widget.dart
+++ lib/flutter_video_widget.dart
@@ -1,7 +1,71 @@
 library flutter_video_widget;
+import 'dart:io';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+/// A  Video Widget
+class VideoWidget extends StatefulWidget {
+  String url;
+  VideoWidgetOptions options = VideoWidgetOptions();
+  bool isNetwork;
+  bool isAsset;
+  bool isFile;
+  File file;
+  VideoWidget.network({@required this.url, this.options}) : this.isNetwork = true, assert(url!=null);
+
+  VideoWidget.file({@required this.file,this.options}) : this.isFile = true, assert(file!=null);
+
+  VideoWidget.asset({@required  this.url, this.options}) : this.isAsset = true, assert(url!= null);
+
+  @override
+  _VideoWidgetState createState() => _VideoWidgetState();
+}
+
+class _VideoWidgetState extends State<VideoWidget> {
+
+  VideoPlayerController videoPlayerController;
+  Widget playerWidget;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializeVideo();
+  }
+
+  initializeVideo() async {
+      if(widget.isNetwork){
+      videoPlayerController = VideoPlayerController.network(widget.url);
+      } else if(widget.isAsset) {
+      videoPlayerController = VideoPlayerController.asset(widget.url);
+      } else if(widget.isFile) {
+      videoPlayerController = VideoPlayerController.file(widget.file);
+      }
+
+      await videoPlayerController.initialize();
+
+      final chewieController = ChewieController(
+        videoPlayerController: videoPlayerController,
+        autoPlay: widget.options.autoplay,
+        looping: widget.options.looping
+      );
+
+      playerWidget = Chewie(
+      controller: chewieController,
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return playerWidget;
+  }
+}
+
+class VideoWidgetOptions {
+  double aspectRatio = 16.4;
+  bool autoplay = true;
+  bool looping = true;
+  VideoWidgetOptions({this.aspectRatio, this.autoplay, this.looping});
 }
